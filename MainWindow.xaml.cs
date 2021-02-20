e8e78aa75ff32316ad1c854f99786a316b5f7034
Jonathan Grehan-Butler <S00199080@mail.itsligo.ie>
2021-02-20 15:53:46 +0000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sem2Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void genbtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (combobx.SelectedItem == warrior)
+            {
+                classnametxtblck.Text = "Warrior";
+            }
+            else if (combobx.SelectedItem == pyromancer)
+            {
+                classnametxtblck.Text = "Pyromancer";
+            }
+            else if (combobx.SelectedItem == sorcerer)
+            {
+                classnametxtblck.Text = "Sorcerer";
+            }
+            else if (combobx.SelectedItem == miracleuser)
+            {
+                classnametxtblck.Text = "Miracle User";
+            }
+            else if (combobx.SelectedItem == dexterityuser)
+            {
+                classnametxtblck.Text = "Dexterity User";
+            }
+        }
+    }
+}
